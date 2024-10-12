e5df67950e45e524bbb3afba0fe42a623700507c
altamash7140 <altamashali7140@gmail.com>
2024-10-12 23:48:37 +0530

    GeminiAI ReactJS

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from './Components/Sidebar/Sidebar'
+import {assets} from './assets/assets'
+import Main from './Components/Main/Main'
+
+const App = () => {
+  return (
+    <>
+    <Sidebar></Sidebar>
+    <Main></Main>
+    </>
+  )
+}
+
+export default App
